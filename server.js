6e07e83681a35d1351e16521d5678e57206f4302
Mayank Tiwari <mike.tiwari@gmail.com>
2015-12-08 21:39:47 -0500

    Add delete todo route

--- server.js
+++ server.js
@@ -71,6 +71,20 @@ app.post('/todos',function(req, res) {
 	res.json(body);
 });
 
+//DELETE /todos/:id
+app.delete('/todos/:id',function(req,res) {
+	var todoId = parseInt(req.params.id,10);
+	//Second argument appears to be base
+	var matchedTodo = _.findWhere(todos, { id: todoId});
+
+	if(!matchedTodo) {
+		res.status(404).json({"error" : " no todos found with that id"});
+	} else {
+		todos = _.without(todos,matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 
 app.listen(PORT, function() {
 	console.log('Express JS is running on port: ' + PORT);
